fe982fbc5100d866459a21669d079c731fb3d1e6
likitha <likithas434@gmail.com>
2021-11-18 09:00:20 +0530

    finished lab4

--- app/src/main/java/com/example/alertbox_lab4/MainActivity.java
+++ app/src/main/java/com/example/alertbox_lab4/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.alertbox_lab4;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void click(View v){
+        AlertDialog.Builder alertdialoguebuilder = new AlertDialog.Builder(this);
+        alertdialoguebuilder.setTitle("Are you sure ?");
+        alertdialoguebuilder.setMessage("Are you sure you want to exit?");
+        alertdialoguebuilder.setCancelable(false);
+
+        alertdialoguebuilder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"You clocked yes",Toast.LENGTH_LONG).show();
+            }
+        });
+        alertdialoguebuilder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"you clicked no",Toast.LENGTH_LONG).show();
+            }
+        });
+        alertdialoguebuilder.setNeutralButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"you clicked cancel",Toast.LENGTH_LONG).show();
+            }
+        });
+        alertdialoguebuilder.create();
+        alertdialoguebuilder.show();
     }
 }
